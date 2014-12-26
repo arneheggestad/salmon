f65ebe29020358aedc1931f81045d68b74bd930d
Arne Heggestad <arne.heggestad@gmail.com>
2014-12-26 12:09:18 -0500

    added round number prompt before each guess

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ prompt.start();
 
 var gameRound = function (gameObj) {
 	simon.newRound(gameObj, function (err, gameObj) {
-		console.log('Take a guess: ');
+		console.log('Round ' + gameObj.round + ' Take a guess: ');
 		prompt.get('guess', function (err, result) {
 			gameObj.guess = result.guess;
 			simon.checkAns(gameObj, function (err, gameObj, correct) {
@@ -31,6 +31,6 @@ console.log('How many choices would you like?');
 prompt.get('choices', function (err, result) {
 	options.choices = result.choices;
 	simon.init(options, function (err, gameObj) {
-		gameRound(gameObj)
+		gameRound(gameObj);
 	});
 });
